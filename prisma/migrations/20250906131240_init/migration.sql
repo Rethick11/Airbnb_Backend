e99c38f7f73f99bc6e21bc36085c917aedf616e0
rethick <rethickpavan11@gmail.com>
2025-09-06 14:14:38 +0100

    created initial migrations

--- prisma/migrations/20250906131240_init/migration.sql
+++ prisma/migrations/20250906131240_init/migration.sql
@@ -1,11 +1,12 @@
 -- CreateTable
-CREATE TABLE `Hotel` (
+CREATE TABLE `Booking` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `address` VARCHAR(191) NULL,
-    `location` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `hotelId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+    `bookingAmount` DOUBLE NOT NULL,
+    `status` ENUM('PENDING', 'CONFIRMED', 'COMPLETED') NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
