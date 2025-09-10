d3ae4e36913caaa6f0e2969cdfe62757229679cc
rethick <rethickpavan11@gmail.com>
2025-09-10 22:51:09 +0100

    added the idempotent api

--- src/repositories/booking.repository.ts
+++ src/repositories/booking.repository.ts
@@ -1,10 +1,51 @@
-import {Prisma,  PrismaClient,  } from '../generated/prisma';
+import { PrismaClient } from "../generated/prisma";
 
 const prisma = new PrismaClient();
 
-export class BookingRepository {
-  async createBooking(data: Prisma.BookingCreateInput) {
-    return prisma.booking.create({ data });
-  }
+interface Data {
+  userId: number;
+  hotelId: number;
+  bookingAmount: number;
+  key: string;
 }
 
+export async function createBooking(data: Data) {
+  const booking = await prisma.booking.create({
+    data: {
+      userId: data.userId,
+      hotelId: data.hotelId,
+      bookingAmount: data.bookingAmount,
+      key: data.key,
+    },
+  });
+
+  return booking;
+}
+
+export async function finalizeBooking(key: string, bookingId: number) {
+  const result = await prisma.$transaction(async (tx) => {
+    try {
+      await tx.idempotencyKey.create({
+        data: {
+          bookingId,
+          key,
+        },
+      });
+    } catch (error: any) {
+      if (error.code === "P2002") {
+        throw new Error("Duplicate request");
+      }
+      throw new Error("Failed to create idempotency key");
+    }
+
+    const booking = await tx.booking.update({
+      where: { id: bookingId },
+      data: { status: "COMPLETED" },
+      include: { IdempotencyKey: true },
+    });
+
+    return booking;
+  });
+
+  return result;
+}
--- src/service/booking.service.ts
+++ src/service/booking.service.ts
@@ -1,9 +1,25 @@
-export async function createBookingService(){
 
-        return "created"
+import { createBooking, finalizeBooking } from "../repositories/booking.repository"
+import { generateUUID } from "../utils/helpers/uuid.helpers"
+
+export  async function  createBookingService( userId : number, bookingAmount : number, hotelId : number) : Promise<{id : number, key : string}>{
+        const key = generateUUID()
+        const booking = await createBooking({
+                userId,
+                bookingAmount,
+                hotelId,
+                key
+        })
+        
+        return {
+                id : booking.id,
+                key : booking.key
+        }
 }
 
-export async function finalizeBookingService(){
+export async function finalizeBookingService(key: string, bookingId: number) {
 
-        return "finalized"
+        const booking = await finalizeBooking(key,bookingId)
+
+        return booking
 }
